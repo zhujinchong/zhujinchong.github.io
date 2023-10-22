540689485630e0c067d0ac9106ca07d688579ff7
mingriyingying <mingriyingying@163.com>
2023-10-22 18:59:00 +0800

    no

--- deploy.sh
+++ deploy.sh
@@ -5,7 +5,7 @@ set -e
 
 # 生成静态文件
 
-yarn run docs:build
+yarn run build
 
 # 进入生成的文件夹
 cd docs/.vitepress/dist
@@ -14,7 +14,7 @@ git init
 git add -A
 git commit -m 'add'
 
-git push -f https://github.com/mingriyingying/mingriyingying.github.io.git master:blog
+git push -f https://github.com/mingriyingying.github.io.git master
 # git push -f git@github.com:你的git名/你的git项目名.git master:你的git分支
 
 cd -
