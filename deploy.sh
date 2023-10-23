e016d3d0eaf3996df6471cdd006697e02288014c
mingriyingying <mingriyingying@163.com>
2023-10-23 16:59:12 +0800

    deploy

--- deploy.sh
+++ deploy.sh
@@ -4,18 +4,17 @@
 set -e
 
 # 生成静态文件
-
-yarn build
+# yarn build
 
 # 进入生成的文件夹
-cd docs/.vitepress/dist
+# cd docs/.vitepress/dist
 
 git init
 git add -A
 git commit -m 'deploy'
 
 # git push -f git@github.com:你的git名/你的git项目名.git master:你的git分支
-git push -f origin deploy
+git push
 
 
 cd -
